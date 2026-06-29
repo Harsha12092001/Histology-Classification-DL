01803c072c573a6061ebe5cb0a7cfebd439a8812
Harshavardini <harshahasini1998@gmail.com>
2026-06-29 16:25:40 +0200

    Fix variable assignment for test_loader in data loading

--- Code/train.py
+++ Code/train.py
@@ -19,7 +19,7 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Training executing on device: {device}")
 
-    train_loader, val_loader, _ = get_loaders(data=config["DATA"], data_path=config["DATA_PATH"], batch_size=config["BATCH_SIZE"])
+    train_loader, val_loader, test_loader   = get_loaders(data=config["DATA"], data_path=config["DATA_PATH"], batch_size=config["BATCH_SIZE"])  #Fix variable assignment for test_loader in data loading
 
     model_class = getattr(models, config["MODEL"])
     model = model_class(in_channels=config["CHANNELS"], num_classes=config["NUM_CLASSES"], drop_rate=config["DROP"], activation_str=config["ACTIVATION"]).to(device)    #added drop_rate and activation as configurable one
